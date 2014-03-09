1d333fbf0c746e35d5184139cebde035af19ad7b
nelsonic <nodecoder@gmail.com>
2014-03-09 00:42:47 +0000

    step one in saving state

--- example/code2hash.js
+++ example/code2hash.js
@@ -1,8 +1,24 @@
+var fs = require('fs'), // basic file iO
+crypto = require('crypto'), // used for sha1 hashing
+i = 0, // increment lines
+lineMap = [],
+hash, lineCount = 0;
+
 // read the file
+fs.readFile('./hello.js', function read(err, data){
+	// split lines by newline character
+	lines = data.toString().split("\n");
 
-// split lines by newline character
+	// hash the bytes of the line 
+	lineCount = lines.length;
+	// console.dir(lines +' | '+lineCount);
+	while(i < lineCount ){
+		hash = crypto.createHash('sha1').update(lines[i]).digest('hex');
+		// store array of hashes
+		lineMap.push(hash);
+		i = i + 1;
+	}
+	console.dir(lineMap);
+});
 
-// hash the bytes of the line 
-
-// store array of hashes
 
