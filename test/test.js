d714523c7051f6140722a4b2fa2244b1c2879e07
nelsonic <nodecoder@gmail.com>
2014-03-09 23:45:18 +0000

    bizzarely the test still passes with assert.equal(1,2) - promises!!

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 var chai = require('chai'),
+	Q      = require('q'),
 	assert = chai.assert, 
 	fs     =  require('fs'),
 	ipex   = require('../lib'),  // our module
@@ -9,19 +10,33 @@ var chai = require('chai'),
 describe('IPEX', function(){
 	describe('Turns code into ', function(){
 		it('Hello World should return an array with two hashes', function(){
-			fs.readFile('./example/hello.js', function read(err, data){
-				if(err) { console.log(" >> ERROR: ",err); }
-				// console.log(data);
-				ipex.hashes(data, function(lineMap){
-					log(lineMap);
-					fs.readFile('./example/hello-hashed.json', function read(err, json){
-											log('this')
-						console.log(json);
-						assert.equal(2, 1);
-					});
-				})
-				// assert.equal();
-			});   
+			Q.fcall(function(){var value4 = 'this'; })
+			.then(function (value4) {
+			    // Do something with value4
+			    console.log("Val: ", value4);
+					assert.equal(2, 1);
+			})
+			.catch(function (error) {
+			    // Handle any error from all above steps
+			    log(">>>> ERROR!")
+			})
+			.done();
+
+
+
+			// fs.readFile('./example/hello.js', function read(err, data){
+			// 	if(err) { console.log(" >> ERROR: ",err); }
+			// 	// console.log(data);
+			// 	ipex.hashes(data, function(lineMap){
+			// 		log(lineMap);
+			// 		fs.readFile('./example/hello-hashed.json', function read(err, json){
+			// 								log('this')
+			// 			console.log(json);
+
+			// 		});
+			// 	})
+			// 	// assert.equal();
+			// });   
 
 		});
 	});
